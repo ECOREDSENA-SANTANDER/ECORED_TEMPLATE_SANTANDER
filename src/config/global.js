2668424c9388deb6128e4a8e9b82cdbf224d62d3
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-05-31 14:53:11 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -186,19 +186,19 @@ export default {
       autores: [
         {
           nombre: 'Zuleidy María Ruiz Torres',
-          cargo: 'Validación y vinculación en plataforma LMS',
+          cargo: 'Validador de Recursos Educativos Digitales',
           centro:
             'Regional Santander - Centro Industrial del Diseño y la Manufactura',
         },
         {
           nombre: 'Luis Gabriel Urueta Alvarez',
-          cargo: 'Validación y vinculación en plataforma LMS',
+          cargo: 'Validador de Recursos Educativos Digitales',
           centro:
             'Regional Santander - Centro Industrial del Diseño y la Manufactura',
         },
         {
           nombre: 'Daniel Ricardo Mutis Gómez',
-          cargo: 'Validación de contenidos accesibles',
+          cargo: 'Evaluador para contenidos inclusivos y accesibles',
           centro:
             'Regional Santander - Centro Industrial del Diseño y la Manufactura',
         },
